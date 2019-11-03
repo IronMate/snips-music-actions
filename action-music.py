e400f329330bb9f7322d4faa8d03184cb378612e
IronMate <38657658+IronMate@users.noreply.github.com>
2019-11-03 20:39:02 +0100

    Update action-music.py

--- action-music.py
+++ action-music.py
@@ -1,52 +1,53 @@
-#!/usr/bin/env python2
- 
-import sys
-import paho.mqtt.client as mqtt
-from spotify_controller import SpotifyController
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-def intent_received(_self, hermes, intent_message):
-    print('Intent {}'.format(intent_message.intent))
-    
-    for (slot_value, slot) in intent_message.slots.items():
-        print('Slot {} -> \n\tRaw: {} \tValue: {}'.format(slot_value, slot[0].raw_value, slot[0].slot_value.value.value))
+from hermes_python.hermes import Hermes, MqttOptions
+import spotify_controller
+import toml
 
-    hermes.publish_end_session(intent_message.session_id, None)
-    
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-
-    client.subscribe("hermes/hotword/default/detected")
-    # Subscribe to intent topic
-    client.subscribe("hermes/intent/#")
-        
-
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    if msg.topic == 'hermes/hotword/default/detected':
-        print("Wakeword detected!")
-    else:
-        try:
-            intent = msg.topic[((len(msg.topic) - msg.topic.index(":")) - 1) * -1:]
-            print("Intent "+intent+" found.")
-            if intent in actions:
-                print("Running intent...")
-                actions[intent](msg.payload)
-        except:
-            e = sys.exc_info()[0]
-            print("Error while trying to invoke intent from topic '"+msg.topic+"': "+e)
+USERNAME_INTENTS = "IronMate"
+MQTT_BROKER_ADDRESS = "localhost:1883"
+MQTT_USERNAME = None
+MQTT_PASSWORD = None
 
 
-spotify = SpotifyController()
+def add_prefix(intent_name):
+    return USERNAME_INTENTS + ":" + intent_name
 
-actions = {"pause":  spotify.pause}
-#, "restartPlaylist", "volume_down", "shuffleMode", "next", "volume_up", "repeatMode", "play", "addPlaylist", "playResource", "previous"]
+def intent_callback_play(hermes, intent_message):
+    sp.start()
+    hermes.publish_end_session(intent_message.session_id, "")
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
 
-client.connect("raspberrypi", 1883, 60)
+def intent_callback_pause(hermes, intent_message):
+    sp.pause()
+    hermes.publish_end_session(intent_message.session_id, "")
 
-client.loop_forever()
+
+def intent_callback_next(hermes, intent_message):
+    sp.next_track()
+    hermes.publish_end_session(intent_message.session_id, "")
+   
+def intent_callback_previous(hermes, intent_message):
+    sp.previous_track()
+    hermes.publish_end_session(intent_message.session_id, "")
+   
+   
+if __name__ == "__main__":
+    sp = Spotify_Controller()
+
+    snips_config = toml.load('/etc/snips.toml')
+    if 'mqtt' in snips_config['snips-common'].keys():
+        MQTT_BROKER_ADDRESS = snips_config['snips-common']['mqtt']
+    if 'mqtt_username' in snips_config['snips-common'].keys():
+        MQTT_USERNAME = snips_config['snips-common']['mqtt_username']
+    if 'mqtt_password' in snips_config['snips-common'].keys():
+        MQTT_PASSWORD = snips_config['snips-common']['mqtt_password']
+    mqtt_opts = MqttOptions(username=MQTT_USERNAME, password=MQTT_PASSWORD, broker_address=MQTT_BROKER_ADDRESS)
+
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intent(add_prefix("play"), intent_callback_play)
+        h.subscribe_intent(add_prefix("pause"), intent_callback_pause)
+        h.subscribe_intent(add_prefix("next"), intent_callback_next)
+        h.subscribe_intent(add_prefix("previous"), intent_callback_previous)
+        h.start()
