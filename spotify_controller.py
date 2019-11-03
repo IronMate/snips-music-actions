d01a08078523af6db377e5bced9f762ce7c66121
IronMate <38657658+IronMate@users.noreply.github.com>
2019-11-03 20:19:13 +0100

    Init spotify_controller

--- spotify_controller.py
+++ spotify_controller.py
@@ -1,27 +1,70 @@
-import sys
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 16:33:41 2019
+
+@author: IronMate
+"""
+
 import spotipy
-import spotipy.util as util
+import spotipy.oauth2 as oauth
+import re
+
+
+USER = 'Username'
+ID = 'User developer id'
+SECRET= 'user developer secret'
+SCOPE= 'user-library-read playlist-read-private playlist-read-collaborative user-modify-playback-state user-read-currently-playing user-read-playback-state'
+URL='http://localhost/'
+
+_re_compiled = re.compile("'name':\s'(.*?)'|'name':\s\"(.*?)\"")
+_auth_finder = re.compile("code=(.*?)$", re.MULTILINE)
+
 
 class SpotifyController:
 
     def __init__(self):
-        self.token = ""
-        if not self.token or self.token == "":
-            #self.token = util.prompt_for_user_token('timo.stamm@gmx.de',
-            #    'user-library-read playlist-read-private playlist-read-collaborative user-modify-playback-state user-read-currently-playing user-read-playback-state',
-            #    client_id='06aa17352eba440db3aa332ca605ae14', 
-            #    client_secret='f84b30beb6934037b44decd90c8a792b',
-            #    redirect_uri='http://localhost/')
+        self.spo = oauth.SpotifyOAuth(client_id=ID,client_secret=SECRET,redirect_uri=URL,scope=SCOPE,cache_path=".cache-{}".format(USER))
+        self.sp = spotipy.Spotify(auth=self.get_token())
 
-            print("Please open the following url edit this file content with the code parameter it returns:\r\n")
-            print("https://accounts.spotify.com/authorize?scope=playlist-read-collaborative+playlist-read-private+user-library-read+user-modify-playback-state+user-read-currently-playing+user-read-playback-state&redirect_uri=http%3A%2F%2Flocalhost%2F&response_type=code&client_id=06aa17352eba440db3aa332ca605ae14\r\n")
+    def spot(self, token):
+        self.token = token
+        self.sp = spotipy.Spotify(auth=token)
+        return self.sp
 
-            exit
+    def refresh_token(self):
+        cached_token = self.spo.get_cached_token()
+        is_it_expired = self.spo.is_token_expired(cached_token)
+        if is_it_expired is True:
+            refreshed_token = cached_token['refresh_token']
+            new_token = self.spo.refresh_access_token(refreshed_token)  
+            self.sp = spotipy.Spotify(auth=new_token)
+            return new_token
 
-        self.sp = spotipy.Spotify(auth=self.token)
+    def get_token(self):
+        token_info = self.spo.get_cached_token()
+        if token_info:
+            access_token = token_info['access_token']
+            return access_token
+        else:
+            print("New One")
+            auth = self.spo.get_authorize_url()
+            print(auth)
+            auth_url = input('Click the link above and copy and paste the url here: ')
+            _re_auth = re.findall(_auth_finder, auth_url)
+            access_token = self.spo.get_access_token(_re_auth[0])
+            return access_token
+
+    def get_sp(self):
+        return self.sp
     
     def pause(self):
         self.sp.pause_playback()
 
     def next_track(self):
         self.sp.next_track()
+        
+    def previous_track(self):
+        self.sp.previous_track()
+        
+    def start(self):
+        self.sp.start_playback(device_id='Device to play on')
