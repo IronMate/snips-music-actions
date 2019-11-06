f3c895a1657471236fe860fec9489961711e6920
IronMate <38657658+IronMate@users.noreply.github.com>
2019-11-06 17:34:32 +0100

    added read_config_file()

--- init.py
+++ init.py
@@ -3,8 +3,16 @@
 
 import spotify_controller
 import configparser
-import action_music as am
 
+def read_configuration_file():
+    try:
+        cp = configparser.ConfigParser()
+        with io.open("config.ini", encoding="utf-8") as f:
+            cp.read_file(f)
+        return {section: {option_name: option for option_name, option in cp.items(section)}
+                for section in cp.sections()}
+    except (IOError, configparser.Error):
+        return dict()
 
 
 if __name__== "__main__":
