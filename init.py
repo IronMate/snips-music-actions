57dc9368af64c5114206653a13e05c197ca373c7
IronMate <38657658+IronMate@users.noreply.github.com>
2019-11-06 17:21:38 +0100

    Create init.py

---
+++ init.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import spotify_controller
+import configparser
+import action-music as am
+
+
+
+if __name__== "__main__":
+    sc = spotify_controller.SpotifyController(am.read_configuration_file())
+
